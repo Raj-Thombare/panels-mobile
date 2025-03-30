530ba68e73f79f57041c8224f07a77fbfe16af1b
Raj Thombare <rajthombare.dev@gmail.com>
2025-03-30 11:53:19 +0530

    basic navigation & tabs

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function explore() {
+  return (
+    <SafeAreaView>
+      <Text>Explore page</Text>
+      <Link href={"/accountinfo"}>
+        <Text>Account Information</Text>
+      </Link>
+    </SafeAreaView>
+  );
+}
